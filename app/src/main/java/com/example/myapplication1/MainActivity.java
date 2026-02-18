9e254a7101524780f657bd97718bddced03266cc
Anil Soni Jewellery project <jay08jha@gmail.com>
2026-02-18 16:29:33 +0530

    update new code

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,9 +22,14 @@ public class MainActivity extends AppCompatActivity {
         );
 
         // Button: View Customer
-        findViewById(R.id.btnViewCustomer).setOnClickListener(v ->
-                startActivity(new Intent(MainActivity.this, customer_pending_amount.class))
-        );
+        Button btnViewCustomer = findViewById(R.id.btnViewCustomer);
+
+        btnViewCustomer.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MainActivity.this, CustomerListActivity.class);
+            startActivity(intent);
+
+        });
 
 
 
